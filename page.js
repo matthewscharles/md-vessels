34b6b224de698669ceaff0bbf0eb176128c27995
Charles Matthews <charles0matthews@gmail.com>
2022-02-18 08:54:36 -0500

    testing

--- page.js
+++ page.js
@@ -11,12 +11,17 @@
             x.setAttribute('class','red');
             x.id = `original${i}`
             console.log(x.children.length)
+            // check length of children and apply a separate process, remove from loop
+
             // if(x.children.length>0)console.log(Array.from(x.children).filter(item=>item.tagName == 'UL'))
             // if(x.innerHTML.split('<ul>').length>1)console.log(x.innerHTML.split('<ul>')[1].split('</ul>')[0])
+
             let listItem =  patcher.appendChild(document.createElement('code'));
-            listItem.innerHTML = JSON.stringify(Vessels.parseObject(x.innerHTML));
-            console.log();
+            let newObject = Vessels.parseObject(x.innerHTML);
+            console.log(newObject.id)
+            listItem.innerHTML = JSON.stringify(newObject);
             listItem.id = `item${i}`
+            output[newObject.id] = newObject;
         })
-        document.getElementById('object').innerHTML += JSON.stringify(Vessels.parseObject('#osc.Oscillator frequency:100 type:sawtooth'));
+        document.getElementById('object').innerHTML += JSON.stringify(output);
     }
