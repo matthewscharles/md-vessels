a9fd3c9d436d32ebf82cb1b11f03aebde5275550
Charles Matthews <charles0matthews@gmail.com>
2022-02-17 20:38:17 -0500

    stringify display

--- page.js
+++ page.js
@@ -11,11 +11,11 @@
             x.setAttribute('class','red');
             x.id = `original${i}`
             console.log(x.children.length)
-            if(x.children.length>0)console.log(Array.from(x.children).filter(item=>item.tagName == 'UL'))
-            if(x.innerHTML.split('<ul>').length>1)console.log(x.innerHTML.split('<ul>')[1].split('</ul>')[0])
-            let listItem =  patcher.appendChild(document.createElement('button'));
-            listItem.innerHTML = x.innerHTML;
-            console.log(JSON.stringify(Vessels.parseObject(x.innerHTML)));
+            // if(x.children.length>0)console.log(Array.from(x.children).filter(item=>item.tagName == 'UL'))
+            // if(x.innerHTML.split('<ul>').length>1)console.log(x.innerHTML.split('<ul>')[1].split('</ul>')[0])
+            let listItem =  patcher.appendChild(document.createElement('code'));
+            listItem.innerHTML = JSON.stringify(Vessels.parseObject(x.innerHTML));
+            console.log();
             listItem.id = `item${i}`
         })
         document.getElementById('object').innerHTML += JSON.stringify(Vessels.parseObject('#osc.Oscillator frequency:100 type:sawtooth'));
